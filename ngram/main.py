277f41d47dee6540caff8419f7bc0f6fa49135d9
vect0r <vectoreddd>
2025-11-21 22:15:06 +0100

    feat: ngram exercise started

---
+++ ngram/main.py
@@ -0,0 +1,12 @@
+import utils
+
+FILE_1 = "POL0005_beczkowska_kedy-droga.xml"
+FILE_2 = "POL0006_beczkowska_w-mieszczanskim-gniezdzie.xml"
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ ngram/utils.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as ET
+import re
+import random
+from collections import Counter
+
+FILE_1 = "POL0005_beczkowska_kedy-droga.xml"
+FILE_2 = "POL0006_beczkowska_w-mieszczanskim-gniezdzie.xml"
+
+def parse_text(path: str) -> list:
+    try:
+        tree = ET.parse(path)
+        root = tree.getroot()
+        text = "".join(root.itertext())
+        text = text.lower()
+        words = re.findall(r'\b\w+\b')
+        return words
+    
+    except FileNotFoundError:
+        print(f"File was not found on specified path: {path}")
