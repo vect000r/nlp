85ba7c81fcbbdadc53e5c5a8a20ba18d152eecc1
vect0r <vectoreddd>
2025-12-16 22:16:56 +0100

    feat: encoding function finished

--- gpttokenizer/basic_tokenizer.py
+++ gpttokenizer/basic_tokenizer.py
@@ -1,8 +1,19 @@
 class BasicTokenizer:
-    def __init__(self, text, ids, vocab_size):
-        self.text = text
-        self.ids = ids
-        self.vocab_size = vocab_size
+    def __init__(self):
+        self.vocab_size = 1024
+        self.merges ={}
+
+    def get_stats(self, token_ids):
+        counts = {}
+        
+        for pair in zip(token_ids, token_ids[1:]):
+            counts[pair] = counts.get(pair, 0) + 1
+        
+        return counts
+
+
+    def merge(self, token_ids, pair, new_index):
+        pass
 
     def train(self):
         """
@@ -10,11 +21,22 @@ class BasicTokenizer:
         """
         pass
 
-    def encode(self):
+    def encode(self, text):
         """
         Encodes given text.
         """
-        pass
+        token_ids = list(text.encode('utf-8'))
+        
+        while len(token_ids) >= 2:
+            stats = self.get_stats(token_ids)
+            pair = min(stats, key = lambda x: self.merges.get(x, float('inf')))
+
+            if pair not in self.merges:
+                break
+            index = self.merges[pair]
+            token_ids = self.merge(token_ids, pair, index)
+        return token_ids
+    
 
     def decode(self):
         """
